2c92bc3f894a76914f0e7c6a871caff4e0739dee
pjh <hstcsys30@naver.com>
2020-08-23 13:29:59 +0900

    test

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,73 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MemberServiceTest {
+    
+//    MemberService memberService = new MemberService();
+    //MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    //왜 이렇게 하는거죵???,,, 이해가 안되용 ㅜ
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("박종희");
+        
+        //when
+        Long saveId = memberService.join(member);
+        
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        
+    }
+    
+    @Test
+    void 중복_회원_예외 () {
+        //given
+        Member member1 = new Member();
+        member1.setName("박종희");
+
+        Member member2 = new Member();
+        member2.setName("박종희");
+
+        //when
+        memberService.join(member1);
+/*
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("");
+        }
+*/
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이름 중복~");
+
+        //then
+    }
+}
