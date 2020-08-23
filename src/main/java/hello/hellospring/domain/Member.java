d563c2e401922e4e494e1ba21ffb9fd4152372b2
pjh <hstcsys30@naver.com>
2020-08-23 20:30:53 +0900

    jpa1

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,17 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//객체와 레파지토리를 매핑 한다 -> JPA
+@Entity
 public class Member {
 
-    private long id;
+    @Id @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private long id;//db가 알아서 생성 -> identity
+
     private String name;
 
     public long getId() {
