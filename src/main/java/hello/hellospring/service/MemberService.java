543bdb440cfc979a535a8a58d907f88cbd9b4698
park jonghee <prog.pjh@gmail,com>
2020-08-23 12:42:41 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,48 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository= new MemoryMemberRepository();
+
+    /*
+        회원가입
+        같은 이름의 회원은 가입이 안된다.
+     */
+    public Long join(Member member){
+        //메소드로 뺴는 단축키 알아오기
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //        Optional<Member> result = memberRepository.findByName(member.getName());
+//        // m -> 이런거 처음써봐요..;;
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException();
+//        });
+
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이름 중복~");
+                });
+    }
+
+    /*
+    * 전체 회원 조회
+    * */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
